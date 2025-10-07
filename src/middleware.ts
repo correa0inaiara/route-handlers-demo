e45c34d5cc0c8e179a819dabe98fde541f3ddd72
Naiara Correa <correa0inaiara@gmail.com>
2025-10-07 10:41:54 -0300

    middleware

---
+++ src/middleware.ts
@@ -0,0 +1,26 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(request: NextRequest) {
+
+    const response = NextResponse.next()
+    const themePreference = request.cookies.get('theme')
+
+    if (!themePreference) {
+        response.cookies.set('theme', 'dark')
+    }
+
+    response.headers.set('custom-header', 'custom-value')
+
+    return response
+
+    // return NextResponse.redirect(new URL('/', request.url))
+
+    // if (request.nextUrl.pathname === '/profile') {
+    //     return NextResponse.rewrite(new URL("/hello", request.nextUrl)) // the url remains /profile, but the route is /hello
+    // }
+}  
+
+// export const config = {
+//     matcher: '/profile'
+// }
